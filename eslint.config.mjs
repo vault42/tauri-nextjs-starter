964a72f8f76f53fe90c05ed30facbcbdbb5a69ca
42arch <rend42@163.com>
2026-05-11 14:12:26 +0800

    Update Node.js version in CI workflow and enhance ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,17 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-  nextjs: true,
-  rules: {
-    'node/prefer-global/process': 'off',
+export default antfu(
+  {
+    nextjs: true,
+    gitignore: true,
+    rules: {
+      'node/prefer-global/process': 'off',
+    },
   },
-})
+  {
+    ignores: [
+      'src-tauri/target/**',
+      'src-tauri/gen/**',
+    ],
+  },
+)
